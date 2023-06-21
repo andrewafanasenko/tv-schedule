a111a075b27e20fc88d04089653a39de29ebc9b2
Andrii Afanasenko <andrii.afanasenko@gmail.com>
2023-06-22 00:07:13 +0300

    Added new packages, small refactoring

--- app/src/main/java/com/example/tvschedule/presentation/MainActivity.kt
+++ app/src/main/java/com/example/tvschedule/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.tvschedule
+package com.example.tvschedule.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,14 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.tvschedule.ui.theme.TVScheduleTheme
+import com.example.tvschedule.presentation.ui.theme.TVScheduleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TVScheduleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
