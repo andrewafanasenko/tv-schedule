368d3d73170cbc325d81c8f6d489666cab1c9e91
Andrii Afanasenko <andrii.afanasenko@gmail.com>
2023-11-06 00:06:50 +0200

    Libraries and dependencies update

--- app/src/main/java/com/example/tvschedule/data/db/Migrations.kt
+++ app/src/main/java/com/example/tvschedule/data/db/Migrations.kt
@@ -6,22 +6,22 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
 
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE show ADD COLUMN rating DOUBLE NULL DEFAULT NULL")
-        database.execSQL("ALTER TABLE show ADD COLUMN averageRuntime INTEGER NOT NULL DEFAULT 0")
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE show ADD COLUMN rating DOUBLE NULL DEFAULT NULL")
+        db.execSQL("ALTER TABLE show ADD COLUMN averageRuntime INTEGER NOT NULL DEFAULT 0")
     }
 }
 
 val MIGRATION_2_3 = object : Migration(2, 3) {
 
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE show ADD COLUMN genres TEXT NOT NULL DEFAULT ''")
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE show ADD COLUMN genres TEXT NOT NULL DEFAULT ''")
     }
 }
 
 val MIGRATION_3_4 = object : Migration(3, 4) {
 
-    override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE show ADD COLUMN originalCoverUrl TEXT NOT NULL DEFAULT ''")
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE show ADD COLUMN originalCoverUrl TEXT NOT NULL DEFAULT ''")
     }
 }
